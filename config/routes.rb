06a610f72d206aeb92e1b013545021eeb96f6bcb
Robbie Clutton <robert.clutton@gmail.com>
2013-05-30 20:54:08 -0400

    Homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Fuck::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  #
+
+  root :to => 'home#index'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
