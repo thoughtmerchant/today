3fa16a8322f8fc94c6a5e94317e036e40a5e7423
Robbie Clutton <robert.clutton@gmail.com>
2013-06-06 20:34:46 -0400

    test fix

--- spec/controllers/ratings_controller_spec.rb
+++ spec/controllers/ratings_controller_spec.rb
@@ -5,7 +5,7 @@ describe RatingsController do
     let(:params) { { name: 'foo', score: '1' } }
 
     it 'creates a rating' do
-      Rating.should_receive(:create).with(params.with_indifferent_access)
+      Rating.should_receive(:create!).with(params.with_indifferent_access)
       post :create, rating: params
       expect(response).to be_success
       expect(response.body).to eq(' ')
